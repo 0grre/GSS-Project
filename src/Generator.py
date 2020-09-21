041e82c030642aa71611edad1ed5e221daaf8e74
Jean-Baptiste Loup <jbloup@boresa.fr>
2020-09-21 15:46:10 +0200

    first commit

---
+++ src/Generator.py
@@ -0,0 +1,32 @@
+import markdown2
+import os
+import sys
+
+get_MD = sys.argv[1]
+give_HTML = sys.argv[2]
+
+if not os.path.exists(give_HTML):
+    os.makedirs(give_HTML)
+
+list_files_MD = os.listdir(get_MD)
+
+for n in list_files_MD:
+    f = open(get_MD + "/" + n, "r")
+    page_rendered = markdown2.markdown(f.read())
+    n = n[:-3]
+    page_rendered_file = open(give_HTML + "/" + n + ".html", "w")
+    head ="""<!doctype html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="./assets/css/style.css" rel="stylesheet" media="all" type="text/css"> 
+        <title>""" + n + """</title>
+    </head>
+    <body>"""
+    footer ="""</body>
+    </html>"""
+    page_rendered_file.write(head+page_rendered+footer)
+    page_rendered_file.close()
